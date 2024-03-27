5156db2d421ed1ba56faade60b90517f9a9c0fb8
ali <aliupgradeup@gmail.com>
2024-03-27 18:35:10 +0800

    feat: 第一次提交，注册接口实现，环境变量抽离

---
+++ src/email/email.module.ts
@@ -0,0 +1,10 @@
+import { Global, Module } from '@nestjs/common';
+import { EmailService } from './email.service';
+import { EmailController } from './email.controller';
+
+@Global()
+@Module({
+  controllers: [EmailController],
+  providers: [EmailService],
+})
+export class EmailModule {}
