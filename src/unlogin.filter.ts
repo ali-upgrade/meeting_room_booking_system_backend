99327d9c2c4572ac46f4ea8309e7a0c05ed241c5
ali <aliupgradeup@gmail.com>
2024-03-30 22:33:00 +0800

    feat: 响应报错信息统一格式，添加调试功能

---
+++ src/unlogin.filter.ts
@@ -0,0 +1,28 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+export class UnLoginException {
+  message: string;
+
+  constructor(message?) {
+    this.message = message;
+  }
+}
+
+@Catch(UnLoginException)
+export class UnloginFilter implements ExceptionFilter {
+  catch(exception: UnLoginException, host: ArgumentsHost) {
+    const res = host.switchToHttp().getResponse<Response>();
+
+    res.json({
+      code: HttpStatus.UNAUTHORIZED,
+      message: 'fail',
+      data: exception.message || '用户未登录',
+    });
+  }
+}
